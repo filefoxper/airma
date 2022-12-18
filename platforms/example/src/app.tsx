9debf87c57b102cb26724cd40c0337b9f3b102d8
wangyi <wang.yi@darcytech.com>
2022-12-18 17:38:48 +0800

    16.0.1

--- platforms/example/src/app.tsx
+++ platforms/example/src/app.tsx
@@ -1,167 +1,175 @@
 import React, { memo, useEffect, useState } from 'react';
 import {
-  RequiredModelProvider,
-  useRequiredModel,
-  factory,
-  useModel,
-  useControlledModel,
-  useSelector,
-  shallowEqual,
-  useRequiredModelState, useLocalSelector
+    RequiredModelProvider,
+    useRequiredModel,
+    factory,
+    useModel,
+    useControlledModel,
+    useSelector,
+    shallowEqual,
+    useRequiredModelState, useLocalSelector
 } from '@airma/react-state';
 
 const counter = (count: number = 0) => {
-    console.log(count)
-  return {
-    count,
-    isNegative: count < 0,
-    increase: () => count + 1,
-    decrease: () => count - 1
-  };
+    console.log(count,1)
+    return {
+        count,
+        isNegative: count < 0,
+        increase: () => count + 1,
+        decrease: () => count - 1
+    };
 };
 
 const modelFactory = {
-  counter: factory(counter, 0)
+    counter: factory(counter, 0)
 };
 
 const Increase = memo(() => {
-  const { increase } = useRequiredModel(modelFactory.counter);
-  return <button onClick={increase}>+</button>;
+    const { increase } = useRequiredModel(modelFactory.counter);
+    return <button onClick={increase}>+</button>;
 });
 
 const Decrease = memo(() => {
-  const { decrease } = useRequiredModel(modelFactory.counter);
-  return <button onClick={decrease}>-</button>;
+    const { decrease } = useRequiredModel(modelFactory.counter);
+    return <button onClick={decrease}>-</button>;
 });
 
 const CountValue = memo(() => {
-  // useRequiredModelState(modelFactory.counter, 10);
-  const { count, isNegative } = useRequiredModel(modelFactory.counter);
-  return <span style={isNegative ? { color: 'red' } : undefined}>{count}</span>;
+    useRequiredModelState(modelFactory.counter, 10);
+    const { count, isNegative } = useRequiredModel(modelFactory.counter);
+    return <span style={isNegative ? { color: 'red' } : undefined}>{count}</span>;
 });
+
+const Refresh = memo(()=>{
+    const [v,setV] = useState(12);
+    const { count } = useRequiredModel(modelFactory.counter,v,{refresh:true});
+    return <div>{count}<button onClick={()=>setV(d=>d+1)}>refresh</button></div>
+});
+
 const dm = (d: number = 0) => ({
-  count: d,
-  ddecrease() {
-    return d - 2;
-  },
-  iincrease() {
-    return d + 2;
-  }
+    count: d,
+    ddecrease() {
+        return d - 2;
+    },
+    iincrease() {
+        return d + 2;
+    }
 });
 const PipeCount = memo(() => {
-  const { count, ddecrease, iincrease } = useRequiredModel(
-      modelFactory.counter.pipe(dm)
-  );
-  return (
-      <div>
-        pipe counting:
+    const { count, ddecrease, iincrease } = useRequiredModel(
+        modelFactory.counter.pipe(dm)
+    );
+    return (
         <div>
-          <button onClick={ddecrease}>-</button>
-          <span>{count}</span>
-          <button onClick={iincrease}>+</button>
+            pipe counting:
+            <div>
+                <button onClick={ddecrease}>-</button>
+                <span>{count}</span>
+                <button onClick={iincrease}>+</button>
+            </div>
         </div>
-      </div>
-  );
+    );
 });
 
 const SelectCount = memo(() => {
-  const isNegative = useSelector(modelFactory.counter, counter => {
-    return counter.isNegative;
-  });
-  console.log('render...');
-  return (
-      <div>
-        select counting:
+    const isNegative = useSelector(modelFactory.counter, counter => {
+        return counter.isNegative;
+    });
+    console.log('render...');
+    return (
         <div>
-          <span>{isNegative ? '-' : '+'}</span>
+            select counting:
+            <div>
+                <span>{isNegative ? '-' : '+'}</span>
+            </div>
         </div>
-      </div>
-  );
+    );
 });
 
 const LocalSelectCount = memo(() => {
-  const { count, decrease, increase, test } = useLocalSelector(
-      counter,
-      (instance) => ({
-        ...instance,
-        async test() {
-          await new Promise(r => setTimeout(r, 1000));
-          instance.increase();
-        }
-      })
-  );
-  return (
-      <div>
-        local select counting:
+    const { count, decrease, increase, test } = useLocalSelector(
+        counter,
+        (instance) => ({
+            ...instance,
+            async test() {
+                await new Promise(r => setTimeout(r, 1000));
+                instance.increase();
+            }
+        })
+    );
+    return (
         <div>
-          <button onClick={decrease}>-</button>
-          <span>{count}</span>
-          <button onClick={increase}>+</button>
-          <button onClick={test}>test</button>
+            local select counting:
+            <div>
+                <button onClick={decrease}>-</button>
+                <span>{count}</span>
+                <button onClick={increase}>+</button>
+                <button onClick={test}>test</button>
+            </div>
         </div>
-      </div>
-  );
+    );
 });
 
 function Counting() {
-  const { count, isNegative, increase, decrease } = useModel(
-      modelFactory.counter
-  );
-  return (
-      <div>
-        counting:
+    const { count, isNegative, increase, decrease } = useModel(
+        modelFactory.counter
+    );
+    return (
         <div>
-          <button onClick={decrease}>-</button>
-          <span style={isNegative ? { color: 'red' } : undefined}>{count}</span>
-          <button onClick={increase}>+</button>
+            counting:
+            <div>
+                <button onClick={decrease}>-</button>
+                <span style={isNegative ? { color: 'red' } : undefined}>{count}</span>
+                <button onClick={increase}>+</button>
+            </div>
         </div>
-      </div>
-  );
+    );
 }
 
 function ControlledCounting() {
-  const [c, setC] = useState<number | undefined>(1);
-  const { count, isNegative, increase, decrease } = useControlledModel(
-      modelFactory.counter,
-      c,
-      setC
-  );
-  return (
-      <div>
-        controlled counting:
+    const [c, setC] = useState<number | undefined>(1);
+    const { count, isNegative, increase, decrease } = useControlledModel(
+        modelFactory.counter,
+        c,
+        setC
+    );
+    return (
         <div>
-          <button onClick={decrease}>-</button>
-          <span style={isNegative ? { color: 'red' } : undefined}>{count}</span>
-          <button onClick={increase}>+</button>
+            controlled counting:
+            <div>
+                <button onClick={decrease}>-</button>
+                <span style={isNegative ? { color: 'red' } : undefined}>{count}</span>
+                <button onClick={increase}>+</button>
+            </div>
         </div>
-      </div>
-  );
+    );
 }
 
 function Counter({ index }: { index: number }) {
-  return (
-      <div>
-        counter:{index}
-        <RequiredModelProvider value={modelFactory}>
-          <div>
-            <Decrease />
-            <CountValue />
-            <Increase />
-          </div>
-          <PipeCount />
-          <SelectCount />
-        </RequiredModelProvider>
-        <Counting />
-        <ControlledCounting />
-        <LocalSelectCount />
-      </div>
-  );
+    return (
+        <div>
+            counter:{index}
+            <RequiredModelProvider value={modelFactory}>
+                <div>
+                    <Decrease />
+                    <CountValue />
+                    <Increase />
+                </div>
+                <PipeCount />
+                <SelectCount />
+                <Refresh/>
+            </RequiredModelProvider>
+            <Counting />
+            <ControlledCounting />
+            <LocalSelectCount />
+        </div>
+    );
 }
 
 export default function App() {
-  return (
-      <div>
-        <Counter index={1} />
-      </div>
-  );
+    return (
+        <div>
+            <Counter index={1} />
+        </div>
+    );
 }
