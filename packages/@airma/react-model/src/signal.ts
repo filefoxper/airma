bfb52917433d789b94f1ad917b4b1effd0400272
王一 <wang.yi@darcytech.com>
2025-08-08 00:36:20 +0800

    18.6.0-pre

--- packages/@airma/react-model/src/signal.ts
+++ packages/@airma/react-model/src/signal.ts
@@ -5,6 +5,7 @@ import {
   Model,
   ModelInstance,
   ModelKey,
+  shallowEqual,
   Store
 } from 'as-model';
 import { useEffect, useLayoutEffect, useRef, useState } from 'react';
@@ -14,32 +15,120 @@ import { SignalGenerator } from './type';
 
 function useSignalSubscribeConnection<T extends ModelInstance>(
   signal: SignalGenerator<T>,
-  subscription: (
-    ins: T,
-    act: Action | null,
-    comparator: null | ((i: T) => any[])
-  ) => void | (() => void)
+  subscription: (ins: T, act: Action | null) => void | (() => void)
 ) {
-  const actionsCollectionRef = useRef<null | ((i: T) => any[])>(null);
-  const propertyCollectionRef = useRef<null | ((i: T) => any[])>(null);
+  const actionsCollectionRef = useRef<
+    null | ((i: T) => ((...args: any[]) => any)[])
+  >(null);
+  const changesCollectionRef = useRef<{
+    collections: any[] | null;
+    collector: null | ((i: T) => any[]);
+  }>({ collector: null, collections: null });
+  const destroyRef = useRef<(() => void) | null>(null);
   useEffect(() => {
-    return signal.subscribe(action => {
+    const unsubscribe = signal.subscribe(action => {
+      const actionsCollector = actionsCollectionRef.current;
+      const { collector: changesCollector, collections: changesCollections } =
+        changesCollectionRef.current;
       const { method } = action;
+      const instance = signal();
+      const isActionMethodMatched = (function matchMethod() {
+        if (!actionsCollector) {
+          return true;
+        }
+        const methods = actionsCollector(instance);
+        return (
+          Array.isArray(methods) &&
+          methods.some(m => typeof m === 'function' && m === method)
+        );
+      })();
+      const isChangesMatched = (function matchChanges() {
+        if (changesCollector == null) {
+          return true;
+        }
+        const currentChanges = changesCollector(instance);
+        changesCollectionRef.current.collections = currentChanges;
+        return shallowEqual(changesCollections, currentChanges);
+      })();
+      if (method != null && (!isActionMethodMatched || !isChangesMatched)) {
+        return;
+      }
+      const prevDestroy = destroyRef.current;
+      if (prevDestroy) {
+        prevDestroy();
+      }
+      const destroy = subscription(instance, action);
+      destroyRef.current = destroy ?? null;
     });
+    return () => {
+      unsubscribe();
+      const destroy = destroyRef.current;
+      if (destroy) {
+        destroy();
+      }
+    };
   }, []);
-  return {
-    onActions(collection: (i: T) => ((...args: any[]) => any)[]) {
-      actionsCollectionRef.current = collection;
+  const res = {
+    onActions(collector: (i: T) => ((...args: any[]) => any)[]) {
+      actionsCollectionRef.current = collector;
     },
-    onChanges(collection: (i: T) => any[]) {
-      propertyCollectionRef.current = collection;
+    onChanges(collector: (i: T) => any[]) {
+      changesCollectionRef.current.collector = collector;
+    }
+  };
+  return {
+    onActions(collector: (i: T) => ((...args: any[]) => any)[]) {
+      res.onActions(collector);
+      return {
+        onChanges: res.onChanges
+      };
+    },
+    onChanges(collector: (i: T) => any[]) {
+      res.onChanges(collector);
+      return {
+        onActions: res.onActions
+      };
     }
   };
 }
 
 function useSignalSubscribe<T extends ModelInstance>(
   signal: SignalGenerator<T>
-) {}
+) {
+  const useWatch = function useWatch(
+    subscription: (ins: T, act: Action | null) => void | (() => void)
+  ) {
+    return useSignalSubscribeConnection(signal, (ins, act) => {
+      if (act?.method == null) {
+        return undefined;
+      }
+      return subscription(ins, act);
+    });
+  };
+  const useEffective = function useEffective(
+    subscription: (ins: T, act: Action | null) => void | (() => void)
+  ) {
+    const [wrap, setWrap] = useState<{
+      action: Action | null;
+      instance: T;
+    } | null>(null);
+    const filter = useSignalSubscribeConnection(signal, (ins, act) => {
+      setWrap({ action: act, instance: ins });
+    });
+    useEffect(() => {
+      if (wrap == null) {
+        return () => undefined;
+      }
+      return subscription(wrap.instance, wrap.action);
+    }, [wrap]);
+    return filter;
+  };
+
+  return {
+    useWatch,
+    useEffect: useEffective
+  };
+}
 
 export function useSignal<S, T extends ModelInstance, D extends S>(
   modelLike: Model<S, T> | ModelKey<S, T> | Store<S, T>,
@@ -64,13 +153,8 @@ export function useSignal<S, T extends ModelInstance, D extends S>(
   const signalCallback = function signalCallback() {
     return signal();
   };
-  useSignalSubscribe(signal);
-  signalCallback.useWatch = function useWatch(
-    callback: (ins: T, act: Action | null) => void | (() => void)
-  ) {
-    useEffect(() => {
-      return signal.subscribe(action => {});
-    }, []);
-  };
+  const handler = useSignalSubscribe(signal);
+  signalCallback.useWatch = handler.useWatch;
+  signalCallback.useEffect = handler.useEffect;
   return usePersistFn(signalCallback);
 }
