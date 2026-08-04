072e05f9f68fdc9c01fa309a9f8c5c75a23fdf1f
王一 <wang.yi@darcytech.com>
2026-08-04 20:31:10 +0800

    react-state@18.6.17, fix problem about store or key may be initialized after subscription happens

--- packages/@airma/react-state/test/content/store.test.tsx
+++ packages/@airma/react-state/test/content/store.test.tsx
@@ -1,5 +1,8 @@
 import { act, renderHook } from '@testing-library/react-hooks';
 import { createStore, useModel } from '@airma/react-state';
+import { createSessionStore, provide, useQuery, useResponse, useSession } from '@airma/react-effect';
+import React, { memo, useEffect } from 'react';
+import { render, waitFor } from '@testing-library/react';
 
 const counter = function counter(count: number) {
   const symbol = (function computeSymbol() {
@@ -50,5 +53,38 @@ describe('store 静态库的用法', () => {
       result.current.instance1.increase();
     });
     expect(result.current.instance2.count).toBe(1);
+  });
+
+  test('静态库 store 可初始化多次，初始化状态以最后一次为准', () => {
+    const counterStore = createStore(counter);
+    const { result } = renderHook(() => {
+      const instance1 = useModel(counterStore, 0);
+      const instance2 = useModel(counterStore, 1);
+      return { instance1, instance2 };
+    });
+    expect(result.current.instance2.count).toBe(1);
+  });
+
+  test('当 store 订阅发生时，初始化效应结束，不能继续进行初始化', async () => {
+    const counterStore = createStore(counter);
+
+    const Child = () => {
+      const { count } = useModel(counterStore, 0);
+      return <span data-testid="count">{count}</span>;
+    };
+    const App = provide().to(
+      memo(() => {
+        const { count,increase } = useModel(counterStore, 0);
+        useModel(counterStore, 1);
+        useEffect(()=>{increase()},[]);
+        return <div>{count>1 ? <Child /> : null}</div>;
+      })
+    );
+
+    const {findByTestId} = render(<App />);
+    await waitFor(async () => {
+      const span = await findByTestId('count');
+      expect(span.innerHTML).toBe('2');
+    });
   });
 });
